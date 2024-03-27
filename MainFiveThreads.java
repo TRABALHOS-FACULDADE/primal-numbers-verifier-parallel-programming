3acd5edb2e0b781da68772adb7149339a9cecc59
Felipe Ribeiro <feliper.dev@gmail.com>
2024-03-27 10:21:30 -0300

    first commit

---
+++ MainFiveThreads.java
@@ -0,0 +1,59 @@
+import utils.ImportantFunctions;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class MainFiveThreads {
+
+    static final int TOTAL_THREADS = 5;
+    static final String input = "./assets/input_data.txt";
+    static final String output = "./assets/output_data_five_threads.txt";
+
+    public static void main(String[] args) {
+        ArrayList<Integer> primals = new ArrayList<>();
+        ArrayList<Thread> tasks = new ArrayList<>();
+        long tempoExecucao = 0;
+
+        try {
+            Scanner scanner = new Scanner(new File(input));
+
+            long startTime = System.currentTimeMillis();
+            int linesReaded = 0;
+            while (scanner.hasNextLine()) {
+
+                String line = scanner.nextLine();
+
+                Thread task = new Thread(() -> {
+                    int formattedLineNumber = Integer.parseInt(line);
+                    if (ImportantFunctions.primalVerifier(formattedLineNumber)) {
+                        primals.add(formattedLineNumber);
+                    }
+                });
+
+                tasks.add(linesReaded % TOTAL_THREADS, task);
+                linesReaded++;
+
+                if (linesReaded % TOTAL_THREADS == 0 || !scanner.hasNextLine()) {
+                    tasks.get(linesReaded % TOTAL_THREADS).start();
+
+                    for (int i = 0; i < linesReaded % TOTAL_THREADS; i++) {
+                        try {
+                            tasks.get(i).join();
+                        } catch (InterruptedException e) {
+                            System.out.println(e.getMessage());
+                        }
+                    }
+                }
+            }
+            long endTime = System.currentTimeMillis();
+            tempoExecucao = endTime - startTime;
+            scanner.close();
+        } catch (FileNotFoundException e) {
+            System.err.println("Erro ao ler o arquivo: " + e.getMessage());
+        }
+        ImportantFunctions.printPrimalsList(primals);
+        System.out.println("Tempo de execução: " + tempoExecucao + " ms");
+    }
+}
---
+++ utils/ImportantFunctions.java
@@ -0,0 +1,25 @@
+package utils;
+
+import java.util.ArrayList;
+
+public class ImportantFunctions {
+
+    public static void printPrimalsList(ArrayList<Integer> primalsList) {
+        if (primalsList.isEmpty()) {
+            System.out.println("Lista vazia");
+        }
+        for (int num : primalsList) {
+            System.out.println(num);
+        }
+    }
+
+    public static Boolean primalVerifier(Integer number) {
+        if (number == 2) return true;
+
+        for (int i=2; i<= Math.sqrt(number); i++) {
+            if (number % i == 0) return false;
+        }
+        return true;
+    }
+
+}
